a553263386d0aae64574ccf52ffdf6d9dd111280
JeSuisMouha <60130067+JeSuisMouha@users.noreply.github.com>
2020-01-21 09:23:02 +0100

    Add files via upload

---
+++ site-de-base/bdd/bdd.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+global $bdd;
+
+$mysql_host = "localhost";
+$mysql_login = "root";
+$mysql_password = "usbw";
+$bd_name ="le_vrai_sens";
+$bdd = mysqli_connect($mysql_host, $mysql_login,$mysql_password, $bd_name);
+mysqli_set_charset($bdd, "utf8");
---
+++ site-de-base/bdd/produits.php
@@ -0,0 +1,19 @@
+<?php
+
+	function cree_table_produits() {
+
+		global $bdd; 
+
+		$sql = "CREATE TABLE IF NOT EXISTS produits
+				(id_produits	INT AUTO_INCREMENT,
+				nom				VARCHAR(45) NOT NULL ,
+				prix 			FLOAT NOT NULL,
+				ingredients		VARCHAR(500) NOT NULL,
+				descriptif		VARCHAR(500) NOT NULL,
+				type            VARCHAR(100) NOT NULL,
+				CONSTRAINT pk_produits PRIMARY KEY (id_produits)
+				);
+				";
+		mysqli_query($bdd,$sql);
+		return 0;
+	}
