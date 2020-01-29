498dd4715c54ca09e3be6b96ff7859210f16d75f
Enzo <lahargue.enzo@outlook.fr>
2020-01-29 13:08:50 +0100

    Modification de la page à propos

--- site-de-base/a_propos.php
+++ site-de-base/a_propos.php
@@ -1,19 +1,27 @@
 <?php include_once("header.php"); ?>
 
-<section>
-<div class="row col-sm-8 offset-sm-2">
+<div class="row">
 
-<h1>La Savonnerie</h1>
-<p>
- La Savonnerie Le Vrai Sens est situé dans le cœur du massif de la Chartreuse en Savoie, dans un petit village, à la campagne, à  St Thibaud de couz.</p>
-<p>J’ai 36 ans, mariée, maman de deux enfants, suite à un intérêt grandissant pour améliorer mon mode de vie et celui de ma famille, on se lance dans  une quête de sens vers une vie plus seine et plus respectueuse de l’environnement, ainsi les prises de conscience se sont enchainées :</p>
-<p>D’abords lié notre lieu de vie : construire, oui mais à la campagne avec des matériaux nobles, dans un environnement propice à l’épanouissement de nos enfants.  Notre manière de consommer, comment réduire nos déchets ? Nourrir notre famille : </p>
-<p>oui mais avec les produits de notre jardin issus de la permaculture, bio, respectant l’écosystème et la biodiversité.</p>
-<p>Puis on a continué de se questionner, comment pendre soin de soin ? Se LAVER oui, mais comment ? utiliser des cosmétiques industrielles ? A ce moment là, on se posait la question de l’impact de nos choix sur notre santé et quel est l’impact de nos choix sur l’environnement. </p>
-<p>Ma conscience écologique naissait, j’ai donc trouvé une alternative pour créer moi-même mes produits, d’abords mes produits ménagers puis des savons fabriqués avec la méthode de saponification à froid. Technique de fabrication qui respecte la peau en l’hydratant et la nourrissant sans enlever le film protecteur de la peau.</p>
-<p>Toutes mes choix éclairés nous ont permis de revenir à l’essentiel, nos besoins primaires et ceux de notre famille. Nous avions besoin d’apporter du sens à nos actions, et cela jusque dans la sphère professionnelle, la transition était en train d’opérer… et avec elle naîssait le VRAI SENS!</p>
-<p>Nous sommes producteurs de savons et de cosmétiques de qualité issus d’une fabrication artisanale et responsable.<p>
+	<div class="col-sm-8 offset-sm-2 mt-5 mb-5">
+			<h1>La Savonnerie</h1>
+	</div>
 
-</p>
-</section>
+	<div class="col-sm-8 offset-sm-2 mt-2 mb-2">
+		<p>La Savonnerie Le Vrai Sens est situé dans le cœur du massif de la Chartreuse en Savoie, dans un petit village, à la campagne, à  St Thibaud de couz.</p>
+
+		<p>J’ai 36 ans, mariée, maman de deux enfants, suite à un intérêt grandissant pour améliorer mon mode de vie et celui de ma famille, on se lance dans  une quête de sens vers une vie plus seine et plus respectueuse de l’environnement, ainsi les prises de conscience se sont enchainées :</p>
+
+		<p>D’abords lié notre lieu de vie : construire, oui mais à la campagne avec des matériaux nobles, dans un environnement propice à l’épanouissement de nos enfants.  Notre manière de consommer, comment réduire nos déchets ? Nourrir notre famille : </p>
+
+		<p>oui mais avec les produits de notre jardin issus de la permaculture, bio, respectant l’écosystème et la biodiversité.</p>
+
+		<p>Puis on a continué de se questionner, comment pendre soin de soin ? Se LAVER oui, mais comment ? utiliser des cosmétiques industrielles ? A ce moment là, on se posait la question de l’impact de nos choix sur notre santé et quel est l’impact de nos choix sur l’environnement. </p>
+
+		<p>Ma conscience écologique naissait, j’ai donc trouvé une alternative pour créer moi-même mes produits, d’abords mes produits ménagers puis des savons fabriqués avec la méthode de saponification à froid. Technique de fabrication qui respecte la peau en l’hydratant et la nourrissant sans enlever le film protecteur de la peau.</p>
+
+		<p>Toutes mes choix éclairés nous ont permis de revenir à l’essentiel, nos besoins primaires et ceux de notre famille. Nous avions besoin d’apporter du sens à nos actions, et cela jusque dans la sphère professionnelle, la transition était en train d’opérer… et avec elle naîssait le VRAI SENS!</p>
+
+		<p>Nous sommes producteurs de savons et de cosmétiques de qualité issus d’une fabrication artisanale et responsable.</p>
+	</div>
+
 <?php include_once("footer.php"); ?>
