15b94ba201fd2781686304beece74160dd265b93
Enzo <lahargue.enzo@outlook.fr>
2020-02-13 10:19:38 +0100

    Ajout fichiers admin

---
+++ site-de-base/admin/admin_index.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'admin_header.php';
+?>
+
+  
+
+    
+    <div class="container">
+
+      <h1>Page administrateur</h1>
+      <p>Bienvenue sur la page Administrateur du site "Le Vrai Sens"<br> Vous pouvez naviguer entre les onglets pour acceder aux fonctionnalités</p>
+
+    </div> 
+
+<?php
+include_once 'admin_footer.php';
+?>
---
+++ site-de-base/admin/admin_produits.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'admin_header.php';
+?>
+	<div class="container">
+		<h1>Liste des produits:</h1>
+		<div class="container">	
+		<?php 
+			include_once '../bdd/bdd.php';
+			
+		?>
+		</div>
+	</div>
+
+
+
+
+<?php
+include_once 'admin_footer.php';
+?>
