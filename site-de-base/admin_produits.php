4b0e7aada6d18e8ef413365152ceb59b7de17b7e
vvhitefox <50984199+vvhitefox@users.noreply.github.com>
2020-02-18 09:57:55 +0100

    Add files via upload

--- site-de-base/admin_produits.php
+++ site-de-base/admin_produits.php
@@ -5,7 +5,9 @@ include_once 'vue/stock.php';
 	<div class="container">
 		<h1>Liste des produits:</h1>
 		<div class="container">	
-			<?php echo show_stock(); ?>
+			<?php echo show_stock(); 
+			include $_SERVER['DOCUMENT_ROOT']."/vue/ajouter_savon.php";
+			?>
 		</div>
 	</div>
 
