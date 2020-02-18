1d5524ee5ea3b03d1527b962928c7f4e965a5e3e
vvhitefox <50984199+vvhitefox@users.noreply.github.com>
2020-02-18 13:11:11 +0100

    Update bdd.php

--- site-de-base/bdd/bdd.php
+++ site-de-base/bdd/bdd.php
@@ -5,8 +5,8 @@ session_start();
 global $bdd;
 
 $mysql_host = "localhost";
-$mysql_login = "root";
-$mysql_password = "root";
-$bd_name ="le_vrai_sens";
+$mysql_login = "l3_info_1";
+$mysql_password = "Wqifc34q";
+$bd_name ="l3_info_1";
 $bdd = mysqli_connect($mysql_host, $mysql_login,$mysql_password, $bd_name);
 mysqli_set_charset($bdd, "utf8");
