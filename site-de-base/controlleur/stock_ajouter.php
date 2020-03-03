60798b857785a9ad3ebc3a06d55a70046a8c3537
Enzo <lahargue.enzo@outlook.fr>
2020-03-03 14:26:06 +0000

    Ajout de la partie panier

--- site-de-base/controlleur/stock_ajouter.php
+++ site-de-base/controlleur/stock_ajouter.php
@@ -5,9 +5,10 @@ include_once "../bdd/bdd.php";
 	function ajouterProduit($nom,$prix,$ingredients,$descriptif,$type){
 	global $bdd;
 	
-	$stmt = mysqli_prepare($bdd,"INSERT INTO produits (nom,prix,ingredients,descriptif,type) VALUES (?,?,?,?,?)");
 
-	$stmt->bind_param('sdsss', $nom,$prix,$ingredients,$descriptif,$type);
+	$stmt = mysqli_prepare($bdd,"INSERT INTO produits (nom,prix,ingredients,descriptif,type,`Mise en attente`) VALUES (?,?,?,?,?,?)");
+	$zero = 0;
+	$stmt->bind_param('sdsssi', $nom,$prix,$ingredients,$descriptif,$type,$zero);
 	$stmt->execute();
 	$stmt->close();
 }
