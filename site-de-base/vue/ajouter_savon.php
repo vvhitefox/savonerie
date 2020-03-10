e7ee8d74b55a92d2734cd18315ada51f9888f067
vvhitefox <50984199+vvhitefox@users.noreply.github.com>
2020-03-10 09:48:54 +0100

    changement select dans admin

--- site-de-base/vue/ajouter_savon.php
+++ site-de-base/vue/ajouter_savon.php
@@ -4,6 +4,9 @@
 
 require "../controlleur/stock_ajouter.php";
 require "../controlleur/stock_pause.php";
+require "../vue/select_product_types.php"
+
+
 
 ?>
 	<script>
@@ -15,6 +18,19 @@ require "../controlleur/stock_pause.php";
         document.getElementById("formulaire").style.display="block";
 	}
 
+	function asd2()
+	{
+    if( document.getElementById("type2").style.display == "none"){
+        document.getElementById("type").style.display="none";
+	document.getElementById("type2").style.display="block";
+	document.getElementById("label").style.display="block";}
+    else{
+        document.getElementById("type").style.display="block";
+	document.getElementById("type2").style.display="none";
+	document.getElementById("label").style.display="none";
+	}
+	}
+
 	</script>
 	<button style="margin-left: 80% " onclick="asd(2)">Ajouter un produit</button>
 	<button onclick="asd(1)">X</button>
@@ -23,11 +39,14 @@ require "../controlleur/stock_pause.php";
 		<label for='nom'>nom du produit </label><input class="input-block-level" type="text" id='nom' name='nom'/>
 		<label for='prix'>prix du produit </label><input class="input-block-level" type="text" id='prix' name='prix'/>
 		<label for='ingredients'>ingredients </label><input class="input-block-level" type="text" id='ingredients' name='ingredients'/>
-		<label for='descriptif'>description </label><input class="input-block-level" type="text" id='descriptif' name='descriptif'/>	
-		<label for='type'>type </label><input class="input-block-level" type="text" id='type' name='type'/>
+		<label for='descriptif'>description </label><input class="input-block-level" type="text" id='descriptif' name='descriptif'/>
+		<label for='type'>type</label><input class="input-block-level" type="text" id='type' name='type' style='display: none'/>			
+		<?php echo show_select(); ?>
+		<div onclick="asd2()">+</div>
 		</div>
 		<button class="btn btn-primary btn-lg btn-block"  type="submit">ajouter</button>
 
 
 	</form>
+
 
